22b7330242a1a724accf8224263858985e1d28f0
[jeonmin <[maxsdf@naver.com]>
2022-06-03 11:02:05 +0900

    "슬라이드쇼완성"

--- 금성/js/금성2.js
+++ 금성/js/금성2.js
@@ -1,25 +1,3 @@
-let slides = document.querySelector(".slides");
-let slideimg = document.querySelectorAll(".slides li");
-currentIdx = 0;
-slideCount = slideimg.length;
-prev = document.querySelector(".controller>#prev");
-next = document.querySelector(".controller>#next");
-slideWidth = 150;
-slideMargin = 50;
-
-next.addEventListener("click", function () {
-  if (currentIdx <= slideCount - 1) {
-    slides.style.left = -(currentIdx + 2) * (slideWidth + slideMargin) + "px";
-    slides.style.transition = `${0.5}s ease-out`;
-  }
-});
-
-prev.addEventListener("click", function () {
-  if (currentIdx >= 0) {
-    slides.style.left = -currentIdx * (slideWidth + slideMargin) + "px";
-    slides.style.transition = `${0.5}s ease-out`;
-  }
-});
 let slides2 = document.querySelector(".show_inner");
 let slideimg2 = document.querySelectorAll("#show_inner1 li");
 currentIdx2 = 0;
