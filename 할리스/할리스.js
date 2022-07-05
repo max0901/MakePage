a80d5a75e15a287aa0bcc3a61f8a77329ebd61d6
JM <maxsdf@naver.com>
2022-07-05 15:49:08 +0900

    "할리스 완성"

--- 할리스/할리스.js
+++ 할리스/할리스.js
@@ -1,18 +1,23 @@
 let nav = document.querySelectorAll(".bottomNav_inner>li");
 let navIn = document.querySelectorAll(".bottomNav_inner>li>a");
 let hidden = document.querySelector("#hidden");
+let bnav = document.querySelector(".bottomNav");
 
+bnav.addEventListener("mouseover", () => {
+  hidden.style.visibility = " visible";
+  hidden.style.height = 300 + "px";
+  hidden.style.opacity = 1;
+});
+bnav.addEventListener("mouseout", () => {
+  hidden.style.visibility = " hidden";
+});
 for (let i = 0; i < nav.length; i++) {
   nav[i].addEventListener("mouseover", function () {
-    hidden.style.visibility = "visible";
-    hidden.style.display = "block";
     nav[i].style.color = "white";
     nav[i].style.backgroundColor = "red";
     navIn[i].style.color = "white";
   });
   nav[i].addEventListener("mouseout", function () {
-    hidden.style.visibility = "hidden";
-    hidden.style.display = "none";
     nav[i].style.color = "black";
     nav[i].style.backgroundColor = "white";
     navIn[i].style.color = "black";
