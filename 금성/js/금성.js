010f55304825d9e2be2783045653b7a6af3a63c8
JM <maxsdf@naver.com>
2022-07-08 14:30:20 +0900

    "모르겠다"

--- 금성/js/금성.js
+++ 금성/js/금성.js
@@ -1,6 +1,7 @@
 let slides = document.querySelector(".slides");
+let slides1 = document.querySelectorAll(".slides");
 let slideimg = document.querySelectorAll(".slides li");
-let slideBox = document.querySelector(".slide");
+let slideBox = document.querySelector("#slide");
 
 let prev_btn = document.querySelector(".controller>.prev");
 let next_btn = document.querySelector(".controller>.next");
@@ -28,23 +29,55 @@ let size1 = 50;
 //   }
 // });
 
+// 첫번쨰
 setInterval(() => {
   if (cnt1 < 1000) {
     cnt1++;
     slides.style.transition = "all 4s";
     slides.style.transform = "translateX(" + -(size1 + margin1) * cnt1 + "px)";
   } else if (cnt1 === slideimg.length - 1) {
-    setTimeout(function () {
-      slides.style.transition = "all 4s";
-      cnt1 = 0;
-      slides.style.transform =
-        "translateX(" + -(size1 + margin1) * cnt1 + "px)";
-    }, 0);
+    for (let i = 0; i < 13; i++) {
+      slides.append(slideimg[i].cloneNode(true));
+      // slideimg[i].remove();
+    }
+
+    slides.style.transition = "all 4s";
+    cnt1 = 0;
+    slides.style.transform = "translateX(" + -(size1 + margin1) * cnt1 + "px)";
   }
 }, 500);
-setInterval(() => {
-  for (let i = 0; i < 13; i++) {
-    slides.append(slideimg[i].cloneNode(true));
-    slideimg[i].remove();
-  }
-}, 3000);
+// setInterval(() => {
+//   for (let i = 0; i < 13; i++) {
+//     slides.append(slideimg[i].cloneNode(true));
+
+//     slides1.remove();
+//   }
+// }, 3000);
+
+// 두번쨰
+// setInterval(() => {
+//   if (cnt1 < 1000) {
+//     cnt1++;
+//     for (let i = 0; i < 1000; i++) {
+//       slides1[i].style.transition = "all 4s";
+//       slides1[i].style.transform =
+//         "translateX(" + -(size1 + margin1) * cnt1 + "px)";
+//     }
+//   } else if (cnt1 === slideimg.length - 1) {
+//     setTimeout(function () {
+//       for (let i = 0; i > 1000; i++) {
+//         slides1[i].style.transition = "all 4s";
+//         cnt1 = 0;
+//         slides1[i].style.transform =
+//           "translateX(" + -(size1 + margin1) * cnt1 + "px)";
+//       }
+//     }, 0);
+//   }
+// }, 500);
+// setInterval(() => {
+//   for (let i = 0; i < 13; i++) {
+//     // slides.append(slideimg[i].cloneNode(true));
+//     slideBox.append(slides1[i].cloneNode(true));
+//     slides1[i].remove();
+//   }
+// }, 3000);
